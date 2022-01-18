9fcd5e071452d0df1f0d08387d4d9c3fb731d788
PanG995 <dominik.golik1995@gmail.com>
2022-01-18 11:56:04 +0100

    Add Button.

--- app/src/main/java/pang/tutorials/myage/MainActivity.kt
+++ app/src/main/java/pang/tutorials/myage/MainActivity.kt
@@ -2,10 +2,19 @@ package pang.tutorials.myage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.myButton)
+        val textView1 = findViewById<TextView>(R.id.textView)
+
+        btnClickMe.setOnClickListener{
+            textView1.text = "Haha clik"
+        }
     }
 }
