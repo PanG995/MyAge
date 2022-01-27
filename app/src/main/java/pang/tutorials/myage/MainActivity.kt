ee5035121aef52fa5ddcbcbb1a215281243b7003
PanG995 <dominik.golik1995@gmail.com>
2022-01-27 10:44:48 +0100

    ! change type of layouts

--- app/src/main/java/pang/tutorials/myage/MainActivity.kt
+++ app/src/main/java/pang/tutorials/myage/MainActivity.kt
@@ -10,16 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val btnClickMe = findViewById<Button>(R.id.myButton)
-        val tvMyTextView = findViewById<TextView>(R.id.textView)
-        var timesCliked = 0
-
-        btnClickMe.setOnClickListener{
-            timesCliked += 1
-
-            tvMyTextView.text = timesCliked.toString()
-            Toast.makeText(this, "Hey Dominik", Toast.LENGTH_LONG).show()
-        }
     }
 }
